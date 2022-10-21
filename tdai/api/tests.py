231b5c88582557d8772004b3e1193e9b49f7cb3e
Bui Sy Tu <tubs@tandan.com.vn>
2022-10-21 08:39:03 +0700

    feat: add test case service folder (#9)

--- tdai/api/tests.py
+++ tdai/api/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from api.services import service_variable
 
 # Create your tests here.
+class ServiceVariableTestCase(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_check_variable_type(self):
+        rs = service_variable.check_variable_type('Xin chào', variable_type='String')
+        self.assertEqual(rs, True)
